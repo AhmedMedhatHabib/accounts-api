05f2c3392344b637b019d67c30a0b1964e70021d
System Administrator <you@example.com>
2020-05-20 17:39:41 +0200

    initialize

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv/config');
-// const cors = require('cors');
-
+const cors = require('cors');
+const PORT = process.env.PORT || 8080;
 // Routes
 const userRoute = require('./routes/user');
 
 // DB Connection
 
 mongoose.connect(
-    process.env.BD_CONNECTION, 
-    { 
-        useNewUrlParser: true, 
-        useUnifiedTopology: true 
+    process.env.BD_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     }
 ).catch(error => console.log(error));
 
 // Middlewares
 const app = express();
-app.listen(3000);
-// app.use(cors());
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
+app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRoute);
